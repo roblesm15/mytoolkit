b3b2f38e801bc2e98ad0bbe32397d09e03588d62
Mónica Robles Fontán <75752846+roblesm15@users.noreply.github.com>
2023-01-10 13:58:39 -0500

    some edits to census_url_data.R

--- census_url_data.R
+++ census_url_data.R
@@ -4,25 +4,33 @@ library(janitor)
 
 popest_sya_url <- "https://www2.census.gov/programs-surveys/popest/tables/2020-2021/puerto-rico/asrh/prc-est2021-syasex.xlsx"
 
+popest_sya <- read.xlsx(popest_sya_url, ) #%>% na.omit()
+
+
 popest_sya_tidy <- read.xlsx(popest_sya_url, 
-                             startRow = 4, rows = c(1:93))%>%
-  select(-X2, -X3, -X4, -`2020`, -`2021`) %>%
-  setNames(c("age", 
-             "MALE_2020", "FEMALE_2020",  
-             "MALE_2021", "FEMALE_2021")) %>%
+                             fillMergedCells = T, startRow = 5, check.names = T, 
+                             rows = c(1:93)) %>%
+  select(X1, matches(".1|.2"), -matches("total")) %>%
+  rename(age = X1) %>%
+  rename_with(~gsub(pattern = "\\.2", replacement = "_2021", .x)) %>%
+  rename_with(~gsub(pattern = "\\.1", replacement = "_2020", .x)) %>%
   filter(!(age %in% c("Total")) & !is.na(age)) %>%
   mutate(age = str_remove(age, "."), 
          age = str_remove(age, "\\+")) %>%
   pivot_longer(cols = c(-age), names_to = "gender", values_to = "estimate") %>%
   separate(gender, c("gender","year")) %>%
-  mutate(gender = recode(gender, MALE = "M", FEMALE = "F"))
-  
+  mutate(gender = recode(gender, Male = "M", Female = "F")) %>%
+  mutate(estimate = as.numeric(estimate), 
+         age = as.numeric(age))
+
 popest_muni_url <- "https://www2.census.gov/programs-surveys/popest/datasets/2020-2021/counties/asrh/cc-est2021-agesex-72.csv"
 
 popest_muni_tidy <- read.csv(popest_muni_url, 
                              fileEncoding = "ISO-8859-1") %>%
   select(NAME, YEAR, matches("AGE.+_[FEM|MALE]")) %>%
   rename(municipio = "NAME", year = "YEAR") %>%
+  filter(year != 1) %>%
+  mutate(year = recode(as.character(year),  `2` = "2020", `3` = "2021")) %>% 
   mutate(municipio = str_remove(municipio, " Municipio")) %>%
   pivot_longer(cols = c(-municipio, -year)) %>%
   mutate(name = str_remove(name, "AGE")) %>%
@@ -38,5 +46,8 @@ popest_muni_tidy <- read.csv(popest_muni_url,
   pivot_longer(-c("municipio","gender", "year"), names_to = "age", values_to = "poblacion") %>%
   mutate(start = as.numeric(str_extract(age, "\\d{2}")),
          end = as.numeric(str_remove(age, "\\d{2}"))) %>%
-  select(municipio, start, end, gender, poblacion) 
+  select(municipio, start, end, gender, poblacion, year) 
 
+
+sum(popest_muni_tidy[popest_muni_tidy$year == "2021", ]$poblacion)
+sum(popest_sya_tidy[popest_sya_tidy$year=="2021", ]$estimate)
